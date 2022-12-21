0410dae989966cb551a271f509c0031121aa79ec
edwardvernon <edvernon@hotmail.co.uk>
2022-12-21 19:43:58 +0800

    added details successfully

--- logic.js
+++ logic.js
@@ -1,6 +1,22 @@
 let batuDetails = document.getElementById("batuDetails");
 let seeDetails = document.getElementById("seeDetails");
 
-seeDetails.onclick = function(){
-    batuDetails.style.visibility = 'visible';
+const showAndHide = () =>{
+    if(seeDetails.innerHTML === 'See details'){
+        batuDetails.style.display = 'block';
+        seeDetails.style.backgroundColor = 'darkBlue';
+        seeDetails.style.color = 'white';
+        seeDetails.innerHTML = 'Hide details';
+    } else {
+        batuDetails.style.display = 'none';
+        seeDetails.style.backgroundColor = 'aqua';
+        seeDetails.style.color = 'black';
+        seeDetails.innerHTML = 'See details';
+    }
+    
 }
+
+seeDetails.onclick = showAndHide;
+
+const body = document.body
+body.append("Hello World")
