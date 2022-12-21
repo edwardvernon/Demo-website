9b4606c750fa1b6824bf8c5e78aaf2a6b280304d
edwardvernon <edvernon@hotmail.co.uk>
2022-12-21 18:49:31 +0800

    trying to add logic

---
+++ logic.js
@@ -0,0 +1,6 @@
+let batuDetails = document.getElementById("batuDetails");
+let seeDetails = document.getElementById("seeDetails");
+
+seeDetails.onclick = function(){
+    batuDetails.style.visibility = 'visible';
+}
